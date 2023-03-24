532cc2afceeefffaac0ed48e7b41e4123e41c267
Daniellli <xushaocong@stu.xmu.edu.cn>
2023-03-24 19:14:27 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+'''
+Author: daniel
+Date: 2023-03-24 19:13:38
+LastEditTime: 2023-03-24 19:13:44
+LastEditors: daniel
+Description: 
+FilePath: /semantic_kitti_api-master/setup.py
+have a nice day
+'''
+
+
+
+from setuptools import setup
+
+setup(
+    name='semantic_kitti_api',
+    version='1.0',
+    packages=['semantic_kitti_api'],
+    install_requires=[
+        'numpy',
+        # 添加其他依赖项
+    ],
+)
+
+
+
